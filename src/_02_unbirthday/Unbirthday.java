04bbd0b2753a4f28d2c891f1217bf42593747b56
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-04-14 17:00:46 -0700

    Unbirthday

--- src/_02_unbirthday/Unbirthday.java
+++ src/_02_unbirthday/Unbirthday.java
@@ -3,13 +3,13 @@ package _02_unbirthday;
 import javax.swing.JOptionPane;
 
 public class Unbirthday {
-public static void main(String[] args) {
-	String birthday = 
-	JOptionPane.showInputDialog("What is your birthday(mm/dd)");
-	if(birthday.equals("09/30")) {
-		JOptionPane.showMessageDialog(null, "Happy Birthday!");
-	}else {
-		JOptionPane.showMessageDialog(null, "Happy Unbirthday!");
+	public static void main(String[] args) {
+		String birthday = JOptionPane.showInputDialog("When's your birthday? In the form mm/dd ex. 05/16");
+		if (birthday.equalsIgnoreCase("04/16")) {
+			JOptionPane.showMessageDialog(null, "Happy Birthday!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Merry UNbirthday!");
+		}
 	}
-}
 }
